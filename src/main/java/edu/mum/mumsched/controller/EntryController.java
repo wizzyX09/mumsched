0cd2e7b95293a613aa4fd32b475a6a12df47c03e
Samita Maharjan <samitamaharjan@gmail.com>
2018-01-26 17:02:59 -0600

    mm

--- src/main/java/edu/mum/mumsched/controller/EntryController.java
+++ src/main/java/edu/mum/mumsched/controller/EntryController.java
@@ -1,5 +1,6 @@
 package edu.mum.mumsched.controller;
 
+import edu.mum.mumsched.model.Entry;
 import edu.mum.mumsched.service.IEntryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
