c469a9e1c016c6081572f6ba01852be021ca1945
michellewis <wisletmichel@gmail.com>
2018-01-27 15:50:23 -0600

    Update CourseRepository.java

--- src/main/java/edu/mum/mumsched/repository/CourseRepository.java
+++ src/main/java/edu/mum/mumsched/repository/CourseRepository.java
@@ -9,6 +9,5 @@ import java.util.List;
 @Repository
 public interface CourseRepository extends JpaRepository<Course,Integer> {
 
-    List<Course> findByNameOrSpecializationOrCode(String NameOrSpecializationOrCode);
     Course findByCode(String code);
 }
