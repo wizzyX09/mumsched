bd64b84e31585637b96c12a22766b5fa6117fad2
Samita Maharjan <samitamaharjan@gmail.com>
2018-01-28 17:22:50 -0600

    heroku setup

---
+++ Procfile
@@ -0,0 +1,1 @@
+web java -Dserver.port=$PORT $JAVA_OPTS -jar target/mumsched-0.0.1-SNAPSHOT.jar -Dspring.profiles.active=heroku
--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -1,3 +1,6 @@
 INSERT INTO role (role_id, role) VALUES (1, 'ADMIN');
+INSERT INTO role (role_id, role) VALUES (2, 'FACULTY');
+INSERT INTO role (role_id, role) VALUES (3, 'STUDENT');
+
 INSERT INTO user (user_id, active, email, last_name, name, password) VALUES (1, 1, 'd@d.com', 'Michel', 'Wislet', '$2a$10$M02LFt7qSFjCthQjnnApweltBPmwyxruPYccFyNQrkif9pR554yF6');
 INSERT INTO user_role (user_id, role_id) VALUES (1, 1);
