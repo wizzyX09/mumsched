8755eb2c7b1fbe77fb958db36f98d73970afe26f
Samita Maharjan <samitamaharjan@gmail.com>
2018-01-28 15:12:41 -0600

    user manage: addUserForm

--- src/main/java/edu/mum/mumsched/controller/UserController.java
+++ src/main/java/edu/mum/mumsched/controller/UserController.java
@@ -1,17 +1,26 @@
 package edu.mum.mumsched.controller;
 
 import edu.mum.mumsched.model.User;
+import edu.mum.mumsched.service.IRoleService;
 import edu.mum.mumsched.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private IRoleService iRoleService;
 
     @GetMapping("/allUsers")
     public String findAll(Model model) {
@@ -22,6 +31,14 @@ public class UserController {
     @GetMapping("/newUser")
     public String addUser(Model model) {
         model.addAttribute("user", new User());
+        model.addAttribute("listOfRoles", iRoleService.findAll());
         return "addUserForm";
+    }
+
+    @PostMapping("/saveUser")
+    public String saveUser(@Valid User user, BindingResult bindingResult) {
+        bindingResult.hasErrors();
+        userService.saveUser(user);
+        return "redirect:/allUsers";
     }
 }
---
+++ src/main/java/edu/mum/mumsched/service/IRoleServiceImpl.java
@@ -0,0 +1,20 @@
+package edu.mum.mumsched.service;
+
+import edu.mum.mumsched.model.Role;
+import edu.mum.mumsched.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class IRoleServiceImpl implements IRoleService {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Override
+    public List<Role> findAll() {
+        return roleRepository.findAll();
+    }
+}
