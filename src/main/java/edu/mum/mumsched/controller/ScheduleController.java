0b84dd6e74b938fb9eb2b2e681f941faf805fd99
DESKTOP-NKBSE57\DELL <wisletmichel@gmail.com>
2018-01-28 19:34:58 -0600

    Merge branch 'master' of https://github.com/michellewis/mumsched into wislet
    # Conflicts:
    #	src/main/java/edu/mum/mumsched/model/Section.java
    #	src/main/java/edu/mum/mumsched/model/Student.java

--- src/main/java/edu/mum/mumsched/controller/ScheduleController.java
+++ src/main/java/edu/mum/mumsched/controller/ScheduleController.java
@@ -1,11 +1,8 @@
 package edu.mum.mumsched.controller;
 
 import edu.mum.mumsched.model.Schedule;
-import edu.mum.mumsched.repository.EntryRepository;
-import edu.mum.mumsched.repository.ScheduleRepository;
 import edu.mum.mumsched.service.IEntryService;
 import edu.mum.mumsched.service.IScheduleService;
-import javafx.concurrent.ScheduledService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,29 +15,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/schedule")
 public class ScheduleController {
     @Autowired
-    private IScheduleService iScheduleService;
-    @Autowired
     IEntryService iEntryService;
+    @Autowired
+    private IScheduleService iScheduleService;
 
     @GetMapping("/manage")
-    public String  manage(Model model){
-        model.addAttribute("schedules",iScheduleService.findAll());
+    public String manage(Model model) {
+        model.addAttribute("schedules", iScheduleService.findAll());
         return "schedule/manage";
     }
+
     @GetMapping("/create")
-    public String  createForm(Model model){
+    public String createForm(Model model) {
         System.out.println("/schedule/create/get");
-        model.addAttribute("schedule",new Schedule());
-        model.addAttribute("entries",iEntryService.findEntryWithoutSchedule());
+        model.addAttribute("schedule", new Schedule());
+        model.addAttribute("entries", iEntryService.findEntryWithoutSchedule());
         return "schedule/add";
     }
+
     @GetMapping("/edit")
-    public String  editForm(Model model){
+    public String editForm(Model model) {
         return "redirect:schedule/edit";
     }
+
     @PostMapping("/create")
-    public String  create(@ModelAttribute Schedule schedule){
-        System.out.println("Entry:"+schedule.getEntry().getId());
+    public String create(@ModelAttribute Schedule schedule) {
+        System.out.println("Entry:" + schedule.getEntry().getId());
         return "redirect:schedule/manage";
     }
 
