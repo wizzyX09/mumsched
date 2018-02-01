9a4fb7bbbc1c7ac3c61ec2da20c5e33af855d3f4
Samita Maharjan <samitamaharjan@gmail.com>
2018-02-01 14:54:06 -0600

    user entities

--- src/main/java/edu/mum/mumsched/model/Faculty.java
+++ src/main/java/edu/mum/mumsched/model/Faculty.java
@@ -6,24 +6,14 @@ import java.util.Set;
 @Table(name="faculty")
 public class Faculty extends User {
     private int facultyID;
-<<<<<<< HEAD
-    private String firstName;
-    private String lastName;
-    private Gender gender;
-    private String email;
-=======
 
->>>>>>> 971d3ff... user changes related to inheritance
     @ManyToMany(cascade = CascadeType.ALL)
     @JoinTable(name = "faculty_unwanted_blocks",
             joinColumns = {@JoinColumn(name = "faculty_id")},
             inverseJoinColumns = {@JoinColumn(name = "block_id")})
-<<<<<<< HEAD
     private Set<Block> unwantedBlocks;
-=======
     private Set<Block> preferredBlocks;
 
->>>>>>> 971d3ff... user changes related to inheritance
     @OneToMany(mappedBy = "faculty")
     private Set<Section> sections;
 
@@ -41,55 +31,14 @@ public class Faculty extends User {
         this.facultyID = facultyID;
     }
 
-<<<<<<< HEAD
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Gender getGender() {
-        return gender;
-    }
-
-    public void setGender(Gender gender) {
-        this.gender = gender;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public Set<Block> getUnwantedBlocks() {
         return unwantedBlocks;
     }
 
     public void setUnwantedBlocks(Set<Block> unwantedBlocks) {
         this.unwantedBlocks = unwantedBlocks;
-=======
-    public Set<Block> getPreferredBlocks() {
-        return preferredBlocks;
-    }
 
-    public void setPreferredBlocks(Set<Block> preferredBlocks) {
-        this.preferredBlocks = preferredBlocks;
->>>>>>> 971d3ff... user changes related to inheritance
     }
-
     public Set<Section> getSections() {
         return sections;
     }
--- src/main/java/edu/mum/mumsched/model/Student.java
+++ src/main/java/edu/mum/mumsched/model/Student.java
@@ -8,15 +8,7 @@ import java.util.Set;
 @Table(name="student")
 public class Student extends User {
     private Integer studentId;
-<<<<<<< HEAD
-    private String firstName;
-    private String lastName;
-    @Enumerated(EnumType.STRING)
-    private Gender gender;
-    private String email;
-=======
 
->>>>>>> 971d3ff... user changes related to inheritance
     @ManyToMany(mappedBy = "students")
     private Set<Section> sections;
 
@@ -48,35 +40,6 @@ public class Student extends User {
     public void setEntry(Entry entry) {
         this.entry = entry;
     }
-<<<<<<< HEAD
-
-    public Gender getGender() {
-        return gender;
-    }
-
-    public void setGender(Gender gender) {
-        this.gender = gender;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Entry getEntry() {
-        return entry;
-    }
-
-    public void setEntry(Entry entry) {
-        this.entry = entry;
-    }
-
-    public Set<Section> getSections() {
-        return sections;
-    }
 
     public List<Track> getTracks() {
         return tracks;
@@ -85,6 +48,5 @@ public class Student extends User {
     public void setTracks(List<Track> tracks) {
         this.tracks = tracks;
     }
-=======
->>>>>>> 971d3ff... user changes related to inheritance
+
 }
