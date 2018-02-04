27f2a194a45b8ffa410f1ba51e9cbe74765465c7
Samita Maharjan <samitamaharjan@gmail.com>
2018-02-04 11:52:43 -0600

    faculty page skeleton

--- src/main/java/edu/mum/mumsched/controller/FacultyController.java
+++ src/main/java/edu/mum/mumsched/controller/FacultyController.java
@@ -1,12 +1,35 @@
 package edu.mum.mumsched.controller;
 
+import edu.mum.mumsched.service.ICourseService;
 import edu.mum.mumsched.service.IFacultyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/faculty")
 public class FacultyController {
-    @Autowired
-    IFacultyService iFacultyService;
 
+    @GetMapping("/course")
+    public String displayPreferredCourses(Model model) {
+        return "faculty/course/manage";
+    }
+
+    @GetMapping("/profile")
+    public String displayFacultyProfile(Model model) {
+        return "faculty/profile/manage";
+    }
+
+    @GetMapping("/block")
+    public String displayUnwantedBlocks(Model model) {
+        return "faculty/block/manage";
+    }
+
+    @GetMapping("/schedule")
+    public String viewSchedule(Model model) {
+        return "faculty/schedule/view";
+    }
 }
