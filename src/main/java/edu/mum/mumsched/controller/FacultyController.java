5db872d1cf10f6319eb66e104022ba8f10752556
Samita Maharjan <samitamaharjan@gmail.com>
2018-02-04 19:25:51 -0600

    faculty > add course preference

--- src/main/java/edu/mum/mumsched/controller/FacultyController.java
+++ src/main/java/edu/mum/mumsched/controller/FacultyController.java
@@ -1,26 +1,60 @@
 package edu.mum.mumsched.controller;
 
+import edu.mum.mumsched.model.Block;
+import edu.mum.mumsched.model.Course;
+import edu.mum.mumsched.model.Faculty;
 import edu.mum.mumsched.service.ICourseService;
 import edu.mum.mumsched.service.IFacultyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequestMapping("/faculty")
 public class FacultyController {
 
+    @Autowired
+    ICourseService iCourseService;
+
+    @Autowired
+    IFacultyService iFacultyService;
+
     @GetMapping("/course")
     public String displayPreferredCourses(Model model) {
+        List<Course> courseList = iCourseService.findAll();
+        model.addAttribute("courseList", courseList);
+        model.addAttribute("preferredCourse", new Course());
         return "faculty/course/manage";
+    }
+
+    @PostMapping("/course/add")
+    public String addPreferredCourse(@Valid @ModelAttribute("preferredCourse") Course preferredCourse, BindingResult bindingResult) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String email = auth.getName();
+        Faculty faculty = iFacultyService.findByEmail(email);
+        Course course = iCourseService.findById(preferredCourse.getId());
+        if (faculty != null && course != null) {
+            faculty.getPreferredCourses().add(course);
+            iFacultyService.save(faculty);
+        }
+        return "redirect:/faculty/course";
     }
 
     @GetMapping("/profile")
     public String displayFacultyProfile(Model model) {
         return "faculty/profile/manage";
+    }
+
+    @GetMapping("/profile-update")
+    public String updateFacultyProfile(Model model) {
+        return "faculty/profile/update";
     }
 
     @GetMapping("/block")
