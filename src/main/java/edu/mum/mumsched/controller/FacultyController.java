c5edb5ff61ae0ab2a05b7915917c45b1651d362c
Samita Maharjan <samitamaharjan@gmail.com>
2018-02-04 20:53:45 -0600

    faculty > displayed course preference

--- src/main/java/edu/mum/mumsched/controller/FacultyController.java
+++ src/main/java/edu/mum/mumsched/controller/FacultyController.java
@@ -14,7 +14,9 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 @Controller
 @RequestMapping("/faculty")
@@ -28,22 +30,47 @@ public class FacultyController {
 
     @GetMapping("/course")
     public String displayPreferredCourses(Model model) {
+        // to display list of courses in drop down list
         List<Course> courseList = iCourseService.findAll();
         model.addAttribute("courseList", courseList);
-        model.addAttribute("preferredCourse", new Course());
+
+        // when a course is selected from the drop down list, the id of the selected course
+        // will be set to this course object
+        model.addAttribute("course", new Course());
+
+        // to dispaly list of courses preferred by faculty in a table
+        Faculty faculty = getLoggedInFaculty();
+        Set<Course> courses = faculty.getPreferredCourses();
+        model.addAttribute("preferredCourses", courses);
+
         return "faculty/course/manage";
+
     }
 
     @PostMapping("/course/add")
-    public String addPreferredCourse(@Valid @ModelAttribute("preferredCourse") Course preferredCourse, BindingResult bindingResult) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        String email = auth.getName();
-        Faculty faculty = iFacultyService.findByEmail(email);
+    public String addPreferredCourse(@Valid @ModelAttribute("course") Course preferredCourse, BindingResult bindingResult) {
+        Faculty faculty = getLoggedInFaculty();
         Course course = iCourseService.findById(preferredCourse.getId());
         if (faculty != null && course != null) {
             faculty.getPreferredCourses().add(course);
             iFacultyService.save(faculty);
         }
+        return "redirect:/faculty/course";
+    }
+
+    @GetMapping("/course/delete/{id}")
+    public String deletePreferredCourse(@PathVariable("id") Integer id) {
+        Faculty faculty = getLoggedInFaculty();
+        Set<Course> courses = faculty.getPreferredCourses();
+
+        Iterator<Course> it = courses.iterator();
+        while (it.hasNext()) {
+            Course course = it.next();
+            if (course.getId() == id) {
+                it.remove();
+            }
+        }
+        iFacultyService.save(faculty);
         return "redirect:/faculty/course";
     }
 
@@ -65,5 +92,12 @@ public class FacultyController {
     @GetMapping("/schedule")
     public String viewSchedule(Model model) {
         return "faculty/schedule/view";
+    }
+
+    public Faculty getLoggedInFaculty() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String email = auth.getName();
+        Faculty faculty = iFacultyService.findByEmail(email);
+        return faculty;
     }
 }
